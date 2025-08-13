56ce3ae46d1a6a46d8616764b11e05f686fec56f
cayOcean <cayquelima4@gmail.com>
2025-08-13 20:23:38 -0300

    aula 02

---
+++ Aula-02/crud-no-db/src/main/java/br/com/aweb/crud_no_db/dto/ProductDTO.java
@@ -0,0 +1,38 @@
+package br.com.aweb.crud_no_db.dto;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private Double price;
+
+// construtor padrão
+public ProductDTO(){}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public void setPrice(Double price) {
+    this.price = price;
+}
+
+public Long getId() {
+    return id;
+}
+
+public String getName() {
+    return name;
+}
+
+public Double getPrice() {
+    return price;
+}
+
+// métodos getters e setters
+
+
+}
