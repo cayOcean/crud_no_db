6fdb9c4057c5c504f797f883f706be8d7505c00d
cayOcean <cayquelima4@gmail.com>
2025-08-13 22:33:28 -0300

    sistema produto

---
+++ Aula-02/sistema-produto/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product(name, price, description) VALUES
+('Notebook', 3.500, 'Notebook 16GB RAM, SSD 512GB'),
+('Iphone', 7.500, 'Iphone Zica'),
+('Tablet', 1.200, 'Tela 10, 64GB'),
+('Monitor', 899.0, 'Monitor Full HD 144HZ'),
+('Teclado', 199.0, 'Teclado mecânico RGB');
